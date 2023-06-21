98a6bea0976e2e9e2ffc4ec2556a9c97ce1c7403
iqquee <hisyntax0@gmail.com>
2023-06-21 04:38:04 +0100

    created the facebook method

--- websites.go
+++ websites.go
@@ -1,57 +1,87 @@
 package puppet
 
 import (
-	"fmt"
 	"os"
 	"time"
 )
 
-func (p *Puppet) FacebookScreenShot() {
+func (p *Puppet) FacebookScreenShot(requestURL string) error {
+	p.Webdriver.Get(FacebookLoginRoute)
 
+	username, err := p.Webdriver.FindElement("id", "email")
+	if err != nil {
+		return err
+	}
+	username.SendKeys("your_username")
+
+	password, err := p.Webdriver.FindElement("id", "pass")
+	if err != nil {
+		return err
+	}
+	password.SendKeys("your_password")
+
+	btn, err := p.Webdriver.FindElement("name", "login")
+	if err != nil {
+		return err
+	}
+
+	if err := btn.Click(); err != nil {
+		return err
+	}
+
+	time.Sleep(5 * time.Second)
+
+	p.Webdriver.Get(requestURL)
+
+	pngBase64, err := p.Webdriver.Screenshot()
+	if err != nil {
+		return err
+	}
+
+	fileName := "screenshot" + time.Now().String() + ".png"
+	pngData, _ := os.Create(fileName)
+	pngData.Write([]byte(pngBase64))
+
+	return nil
 }
 
 // GetInstagram is able to get a screen shot off instagram
-func (p *Puppet) InstagramScreenShot(requestURL string) {
+func (p *Puppet) InstagramScreenShot(requestURL string) error {
 	p.Webdriver.Get(InstagramLoginRoute)
 
 	username, err := p.Webdriver.FindElement("name", "username")
 	if err != nil {
-		fmt.Println("error finding element: ", err)
-		return
+		return err
 	}
 	username.SendKeys("your_username")
 
 	password, err := p.Webdriver.FindElement("name", "password")
 	if err != nil {
-		fmt.Println("error finding element: ", err)
-		return
+		return err
 	}
 	password.SendKeys("your_password")
 
-	// Click login button
 	btn, err := p.Webdriver.FindElement("xpath", "//button[contains(.,'Log In')]")
 	if err != nil {
-		fmt.Println("error finding the login button element: ", err)
-		return
+		return err
 	}
 	if err := btn.Click(); err != nil {
-		fmt.Println("error clicking the login button: ", err)
-		return
+		return err
 	}
 
-	// Wait for Instagram to load
 	time.Sleep(5 * time.Second)
 
-	// Navigate to post URL
 	p.Webdriver.Get(requestURL)
 
-	// Take screenshot and save it
 	pngBase64, err := p.Webdriver.Screenshot()
 	if err != nil {
-		fmt.Println("an error occurened while taking screenshot: ", err)
-		return
+		return err
 	}
-	pngData, _ := os.Create("screenshot.png")
+
+	fileName := "screenshot" + time.Now().String() + ".png"
+	pngData, _ := os.Create(fileName)
 	pngData.Write([]byte(pngBase64))
+
+	return nil
 }
 func (p *Puppet) TwitterScreenShot() {}
